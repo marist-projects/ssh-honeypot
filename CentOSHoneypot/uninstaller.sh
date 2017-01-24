d48220f4315d917dc16a55d54d415ecfb3a7d9ab
marist-projects <marcosblakeisle@gmail.com>
2017-01-24 13:03:39 -0500

    Added Uninstaller

---
+++ CentOSHoneypot/uninstaller.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#################################################################################################
+#                         MARIST SSH HONEYPOT CENTOS UNINSTALLER v0.1                           #
+#                            MARIST COLLEGE NETWORKING DEPARTMENT                               #
+#################################################################################################
+
+####################################### Files to Remove #########################################
+rm -r /usr/local/source/openssh
+rm -r /var/log/ssh-honeypot
+rm /usr/local/etc/sshd_config-22
+rm /usr/local/etc/sshd_config-2222
+rm /usr/local/sbin/sshd-22
+rm /usr/local/sbin/sshd-2222
+sed -i "0,/RE/s/Port .*/Port 22/g" /etc/ssh/sshd_config
+service sshd restart
