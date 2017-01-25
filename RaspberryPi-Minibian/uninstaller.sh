c0ea0277871bf557b1d310437e825456b68afef2
dr-crow <James.Crowley1@marist.edu>
2017-01-24 22:25:13 -0500

    Fixed it no more debug

--- RaspberryPi-Minibian/uninstaller.sh
+++ RaspberryPi-Minibian/uninstaller.sh
@@ -17,21 +17,13 @@ sed -i "0,/RE/s/Port .*/Port 22/g" /etc/ssh/sshd_config
 
 echo "Removing Rsyslog Configs......"
 rm /etc/rsyslog.d/00-honeypot.conf
-echo "Removing Rsyslog Configs......1"
 sed -i '/#HONEYPOT CONFIGURATION START/ c\' /etc/rsyslog.conf
-echo "Removing Rsyslog Configs......2"
-sed -i '/\$WorkDirectory \/var\/lib\/rsyslog\/ c\' /etc/rsyslog.conf
-echo "Removing Rsyslog Configs......3"
+sed -i '/\$WorkDirectory \/var\/lib\/rsyslog/ c\' /etc/rsyslog.conf
 sed -i '/\$ActionQueueFileName fwdRule1/ c\' /etc/rsyslog.conf
-echo "Removing Rsyslog Configs......4"
 sed -i '/\$ActionQueueMaxDiskSpace .*/ c\' /etc/rsyslog.conf
-echo "Removing Rsyslog Configs......5"
 sed -i '/\$ActionQueueSaveOnShutdown on/ c\' /etc/rsyslog.conf
-echo "Removing Rsyslog Configs......6"
 sed -i '/\$ActionQueueType LinkedList/ c\' /etc/rsyslog.conf
-echo "Removing Rsyslog Configs......7"
 sed -i '/\$ActionResumeRetryCount -1/ c\' /etc/rsyslog.conf
-echo "Removing Rsyslog Configs......8"
 sed -i '/#HONEYPOT CONFIGURATION END/ c\' /etc/rsyslog.conf
 
 echo "Restarting Rsyslog....."
